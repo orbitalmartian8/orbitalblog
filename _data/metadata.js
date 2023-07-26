8b06120073b0c8af3416a3e81814cd456274b0eb
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-07-26 17:23:38 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "OrbitalMartian - Blog",
+	url: "https://orbitalblog.vercel.app/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Hello World!",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "CJ",
+		email: "linuxgamer@duck.com",
 		url: "https://example.com/about-me/"
 	}
 }
